64d8cc682064a3e1cd3030ac074c0806affe4112
felipe <felipemoreira1@gmail.com>
2022-12-25 18:20:02 -0300

    TabelaTransferencias component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,138 @@
 import PesquisaForm from "./components/pesquisaForm";
-import "./App.css";
+import TabelaTransferencias from "./components/tabelaTransferencias";
 
 function App() {
+  let transferencias = [
+    {
+      id: 1,
+      dataTransferencia: "2019-01-01T07:00:00-02:00",
+      valor: 30895.46,
+      tipo: "DEPOSITO",
+      conta: {
+        id: 1,
+        nomeResponsavel: "Fulano",
+      },
+    },
+    {
+      id: 2,
+      dataTransferencia: "2019-02-03T04:53:27-02:00",
+      valor: 12.24,
+      tipo: "DEPOSITO",
+      conta: {
+        id: 2,
+        nomeResponsavel: "Sicrano",
+      },
+    },
+    {
+      id: 3,
+      dataTransferencia: "2019-05-04T02:12:45-03:00",
+      valor: -500.5,
+      tipo: "SAQUE",
+      conta: {
+        id: 1,
+        nomeResponsavel: "Fulano",
+      },
+    },
+    {
+      id: 4,
+      dataTransferencia: "2019-08-07T02:12:45-03:00",
+      valor: -530.5,
+      tipo: "SAQUE",
+      conta: {
+        id: 2,
+        nomeResponsavel: "Sicrano",
+      },
+    },
+    {
+      id: 5,
+      dataTransferencia: "2020-06-08T04:15:01-03:00",
+      valor: -3241.23,
+      tipo: "TRANSFERENCIA",
+      conta: {
+        id: 1,
+        nomeResponsavel: "Fulano",
+      },
+    },
+    {
+      id: 6,
+      dataTransferencia: "2021-04-01T06:12:04-03:00",
+      valor: 25173.09,
+      tipo: "TRANSFERENCIA",
+      conta: {
+        id: 2,
+        nomeResponsavel: "Sicrano",
+      },
+    },
+    {
+      id: 1,
+      dataTransferencia: "2019-01-01T07:00:00-02:00",
+      valor: 30895.46,
+      tipo: "DEPOSITO",
+      conta: {
+        id: 1,
+        nomeResponsavel: "Fulano",
+      },
+    },
+    {
+      id: 2,
+      dataTransferencia: "2019-02-03T04:53:27-02:00",
+      valor: 12.24,
+      tipo: "DEPOSITO",
+      conta: {
+        id: 2,
+        nomeResponsavel: "Sicrano",
+      },
+    },
+    {
+      id: 3,
+      dataTransferencia: "2019-05-04T02:12:45-03:00",
+      valor: -500.5,
+      tipo: "SAQUE",
+      conta: {
+        id: 1,
+        nomeResponsavel: "Fulano",
+      },
+    },
+    {
+      id: 4,
+      dataTransferencia: "2019-08-07T02:12:45-03:00",
+      valor: -530.5,
+      tipo: "SAQUE",
+      conta: {
+        id: 2,
+        nomeResponsavel: "Sicrano",
+      },
+    },
+    {
+      id: 5,
+      dataTransferencia: "2020-06-08T04:15:01-03:00",
+      valor: -3241.23,
+      tipo: "TRANSFERENCIA",
+      conta: {
+        id: 1,
+        nomeResponsavel: "Fulano",
+      },
+    },
+    {
+      id: 6,
+      dataTransferencia: "2021-04-01T06:12:04-03:00",
+      valor: 25173.09,
+      tipo: "TRANSFERENCIA",
+      conta: {
+        id: 2,
+        nomeResponsavel: "Sicrano",
+      },
+    },
+  ];
+
   return (
     <div className="container">
+      <h3>Transferências</h3>
       <PesquisaForm />
+      <TabelaTransferencias
+        todasTransferencias={transferencias}
+        transferenciasSelecionadas={transferencias}
+      />
     </div>
   );
 }
---
+++ src/components/tabelaTransferencias.jsx
@@ -0,0 +1,71 @@
+const TabelaTransferencias = ({
+  todasTransferencias,
+  transferenciasSelecionadas,
+}) => {
+  const dataFormatada = (input) => {
+    let data = new Date(input),
+      dia = data.getDate().toString(),
+      diaF = dia.length === 1 ? "0" + dia : dia,
+      mes = (data.getMonth() + 1).toString(), //+1 pois no getMonth Janeiro começa com zero.
+      mesF = mes.length === 1 ? "0" + mes : mes,
+      anoF = data.getFullYear();
+    return diaF + "/" + mesF + "/" + anoF;
+  };
+
+  const formataTipo = (transferencia) => {
+    if (transferencia.tipo === "DEPOSITO") {
+      return "Depósito";
+    }
+
+    if (transferencia.tipo === "SAQUE") {
+      return "Saque";
+    }
+
+    if (Number(transferencia.valor) > 0) {
+      return "Transferência Entrada";
+    }
+
+    return "Transferencia Saída";
+  };
+
+  const saldo = (transferencias) => {
+    return transferencias.reduce((soma, transferencia) => {
+      return soma + Number(transferencia.valor);
+    }, 0);
+  };
+
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th colSpan={4}><pre>{"Saldo Total: R$ " +
+              saldo(todasTransferencias) +
+              "\t\tSaldo no período: " +
+              saldo(transferenciasSelecionadas)}</pre>
+          </th></tr>
+          <tr>
+            <td>Dados</td>
+            <td>Valentia</td>
+            <td>Tipo</td>
+            <td>Nome operador transacionado</td>
+          </tr>
+        </thead>
+        <tbody>
+          {transferenciasSelecionadas.map((transferencia) => {
+            return (
+              <tr key={transferencia.id}>
+                <td>{dataFormatada(transferencia.dataTransferencia)}</td>
+                <td>{"R$ " + transferencia.valor}</td>
+                <td>{formataTipo(transferencia)}</td>
+                <td>{transferencia.conta.nomeResponsavel}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default TabelaTransferencias;
